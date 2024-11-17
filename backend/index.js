3847875e8ae02c02d7de649b9bf4b0e2a81cc928
livingstoneandrea <livingstoneandrea7@gmail.com>
2024-11-17 12:40:35 +0300

    added passport configurations

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,52 @@
 import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
+import {GraphQLLocalStrategy, buildContext} from 'graphql-passport'
+
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv'
 
+import passport from 'passport'
+import session from 'express-session'
+import connectMongo from 'connect-mongodb-session'
+
+
 import mergedResolvers from './resolvers/index.js'
 import mergedTypeDefs from "./typeDefs/index.js"
 import { connectDB } from "./db/connectDB.js";
+import { configurePassport } from "./passport/passport.config.js";
 
 dotenv.config();
+configurePassport()
 
 const app = express();
 
 const httpServer = http.createServer(app)
+
+const MongoDBStore = connectMongo(session)
+
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    collection: "sessions"
+})
+
+store.on("error", (err)=> console.log(err))
+
+app.use(session({
+    session: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+        maxAge: 1000*60*60*24*7,
+        httpOnly: true,
+    },
+    store: store
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -29,11 +60,15 @@ await server.start()
 
 app.use(
     '/',
-    cors(),
+    cors({
+        origin:"http://localhost:3000",
+        credentials: true
+    }  
+    ),
     express.json(),
    
     expressMiddleware(server, {
-      context: async ({ req }) => ({ req }),
+      context: async ({ req,res }) => buildContext({ req, res }),
     }),
   );
   
@@ -43,6 +78,6 @@ app.use(
   );
 
   await connectDB()
-  
+
   console.log(`🚀 Server ready at http://localhost:4000/`);
  
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -6,7 +6,7 @@ const userResolver = {
             return users
         },
 
-        user: (_, {userId})=>{
+        user: (_, {userId},)=>{
             return users.find((user)=> user._id === userId)
         }
 
