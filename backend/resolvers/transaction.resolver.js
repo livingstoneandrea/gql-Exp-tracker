9aa4c09e7ce6683d82e0fc368aaa51af90973d7a
livingstoneandrea <livingstoneandrea7@gmail.com>
2024-11-17 15:57:36 +0300

    added resolvers

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,7 +1,75 @@
 
+import Transaction from "../models/transaction.model.js";
+
 const transactionResolver = {
-    Query: {},
-    Mutation: {},
+    Query: {
+        transactions: async (context)=>{
+            try {
+                if (!context.getUser()) throw new Error("Unauthorized");
+
+                const userId = context.getUser()._id;
+
+                const transactions = await Transaction.find({userId})
+
+                return transactions
+
+
+            } catch (err) {
+                console.error("Error getting transactions: ", err);
+                throw new Error("Error getting transactions")
+                
+            }
+        },
+        transaction: async (_, {transactionId},)=>{
+            try {
+                
+                const transaction = await Transaction.findById(transactionId)
+                return transaction
+            } catch (err) {
+                console.error("Error getting transaction: ", err);
+                throw new Error("Error getting transaction")
+            }
+        }
+
+    },
+    Mutation: {
+        createTransaction: async (_parent, {input}, context)=>{
+            
+            try {
+                const newTransaction = new Transaction({
+                    ...input,
+                    userId: context.getUser()._id
+                })
+                await newTransaction.save()
+                return newTransaction
+            } catch (err) {
+                console.error("Error creating transaction: ", err);
+                throw new Error("Error creating transaction")
+            }
+        },
+        updateTransaction: async (_parent, {input}, _context)=>{
+             
+            try {
+                const updateTransaction = await Transaction.findByIdAndUpdate(input.transactionId,input, {new: true})
+
+                return updateTransaction
+            } catch (err) {
+                console.error("Error updating transaction: ", err);
+                throw new Error("Error updating transaction")
+            }
+        },
+        deleteTransaction: async (_parent, {transactionId}, _context)=>{
+            try {
+                const deleteTransaction = await Transaction.findByIdAndDelete(transactionId)
+
+                return deleteTransaction
+            } catch (err) {
+                console.error("Error deleting transaction: ", err);
+                throw new Error("Error deleting transaction")
+            }
+        },
+
+    },
 }
 
 export default transactionResolver
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,18 +1,100 @@
-import {users} from '../dummyData/data.js'
+
+import User from '../models/user.model.js'
 
 const userResolver = {
     Query: {
-        users: ()=> {
-            return users
+        authUser:async (context)=>{
+            try {
+                const user = await context.getUser()
+                return user
+            } catch (err) {
+                console.error("Error in authUser: ", err);
+                throw new Error("Internal server error");
+                
+            }
         },
-
-        user: (_, {userId},)=>{
-            return users.find((user)=> user._id === userId)
+        user:async (_, {userId},)=>{
+            try {
+                const user = await User.findById(userId)
+                return user
+            } catch (err) {
+                console.error("Error in user query: ", err);
+                throw new Error( err.message ||"Error in getting user");
+            }
+            
         }
+        //Todo => add user / transaction relations
 
     },
 
-    Mutation:{}
+    Mutation:{
+        signUp: async (_, {input}, context)=>{
+            try {
+                const {username, name, password, gender}= input
+
+                if (!username || !name || !password || !gender) {
+                    throw new Error("All fields are required")
+                }
+                const existingUser = await User.findOne({username})
+                if (existingUser) {
+                    throw new Error("User already exists")
+                }
+                const salt = await bcrypt.genSalt(10)
+                const hashedPassword = await bcrypt.hash(password, salt)
+
+                const boyprofilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+                const grilprofilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+                const newUser = new User({
+                    username,
+                    name,
+                    password: hashedPassword,
+                    gender,
+                    profilePicture: gender === "male" ? boyprofilePic : grilprofilePic
+                })
+                
+                await newUser.save()
+                await context.login(newUser)
+                return newUser
+
+                
+            } catch (err) {
+                console.log("Error in signUp: ", err);
+                throw new Error(err.message || "Internal server error");
+                
+            }
+        },
+
+        login: async (_, {input}, context) =>{
+            try {
+                const {username, password} = input;
+
+                const {user} = await context.authenticate("graphql-local", {username, password})
+
+                await context.login(user)
+
+                return user
+            } catch (err) {
+                console.error("Error in login:", err);
+                throw new Error(err.message || "internal server error")
+                
+            }
+        },
+        logout: async ( context)=>{
+            try {
+                await context.logout()
+                req.session.destroy((err)=>{
+                    if (err) throw new Error(err.message)
+                })
+                res.clearCookies("connect.sid")
+                return {message: "Log out successfully"}
+
+            } catch (err) {
+                console.error("Error in logout:", err);
+                throw new Error(err.message || "internal server error")
+            }
+        }
+    }
 
 
 }
